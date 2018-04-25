6624cfba03814777011a9badc47fc3091bea9fb8
jurre <0891278@hr.nl>
2018-04-25 13:03:43 +0200

    * BudgetBuddyPage.xaml: Fixed
    * BudgetBuddyPage.xaml.cs:

--- BudgetBuddy/BudgetBuddyPage.xaml.cs
+++ BudgetBuddy/BudgetBuddyPage.xaml.cs
@@ -1,4 +1,4 @@
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace BudgetBuddy
 {
@@ -10,17 +10,16 @@ namespace BudgetBuddy
             InitializeComponent();
         }
 
-<<<<<<< HEAD
-        void Handle_Clicked(object sender, System.EventArgs e)
+        void Text_Click(object sender, System.EventArgs e)
         {
             DisplayAlert("Budget Buddy", "Test", "OK");
-=======
+        }
+
 
         void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
         {
             Rene.Text = string.Format("Je Bent {0:F2}% Rene!", e.NewValue);
             //throw new System.NotImplementedException();
->>>>>>> 8e8aebf... * BudgetBuddyPage.xaml: Ik ben 55.15% Rene!
         }
     }
 }
