1487d3d66b69d2bc2c28552b74cf4e8c005e25b2
jurre <0891278@hr.nl>
2018-04-25 12:35:46 +0200

    * BudgetBuddyPage.xaml: Ik ben 55.15% Rene!
    * BudgetBuddyPage.xaml.cs:
    
    Conflicts:
    	BudgetBuddy/BudgetBuddyPage.xaml
    	BudgetBuddy/BudgetBuddyPage.xaml.cs

--- BudgetBuddy/BudgetBuddyPage.xaml.cs
+++ BudgetBuddy/BudgetBuddyPage.xaml.cs
@@ -1,17 +1,26 @@
-﻿using Xamarin.Forms;
+using Xamarin.Forms;
 
 namespace BudgetBuddy
 {
     public partial class BudgetBuddyPage : ContentPage
     {
+
         public BudgetBuddyPage()
         {
             InitializeComponent();
         }
 
+<<<<<<< HEAD
         void Handle_Clicked(object sender, System.EventArgs e)
         {
             DisplayAlert("Budget Buddy", "Test", "OK");
+=======
+
+        void Handle_ValueChanged(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            Rene.Text = string.Format("Je Bent {0:F2}% Rene!", e.NewValue);
+            //throw new System.NotImplementedException();
+>>>>>>> 8e8aebf... * BudgetBuddyPage.xaml: Ik ben 55.15% Rene!
         }
     }
 }
