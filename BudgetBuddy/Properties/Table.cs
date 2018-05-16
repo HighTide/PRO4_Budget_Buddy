b1f9afe8e440fc001c62b3ab62f435cb83ee6d23
jurre <0891278@hr.nl>
2018-05-16 11:26:43 +0200

    Created Tables

--- BudgetBuddy/Properties/Table.cs
+++ BudgetBuddy/Properties/Table.cs
@@ -4,13 +4,36 @@ namespace BudgetBuddy.Properties
                      
                      
 {
-	public class SQLSettings
-    {
-
+	public class SQL_Settings
+	{
+		[PrimaryKey]
         public string Name { get; set; }
 
         [MaxLength(255)]
         public string Value { get; set; }
     }
+    
+	public class SQL_Uitgaven
+    {
+        [PrimaryKey]
+		public float Date { get; set; }
+
+        [MaxLength(255)]
+        public double Value { get; set; }
+
+		[MaxLength(255)]
+		public string CatCategory { get; set; }
+
+		[MaxLength(255)]
+        public string Name { get; set; }
+    }
+
+	public class SQL_Category 
+	{
+        [PrimaryKey]
+        public string Name { get; set; }
+    }
+
+
 
 }
