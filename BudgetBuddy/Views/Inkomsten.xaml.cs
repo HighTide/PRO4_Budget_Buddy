ce7da440f7e020244517395990def1a6fa06b22f
floorvermaat <floorvermaat@hotmail.com>
2018-05-23 00:24:52 +0200

    added async and alert

--- BudgetBuddy/Views/Inkomsten.xaml.cs
+++ BudgetBuddy/Views/Inkomsten.xaml.cs
@@ -10,29 +10,30 @@ using SQLite;
 
 namespace BudgetBuddy.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Inkomsten : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inkomsten : ContentPage
+    {
         private SQLiteAsyncConnection _connection;
 
-		public Inkomsten ()
-		{
-			InitializeComponent ();
+        public Inkomsten()
+        {
+            InitializeComponent();
 
             _connection = DependencyService.Get<ISQLiteDb>().GetConnection();
-		}
+        }
 
         void Entry_Completed(object sender, EventArgs e)
         {
             var text = ((Entry)sender).Text; //cast sender to access the properties of the Entry
         }
 
-        private void Button_OnClicked(object sender, EventArgs e)
+        private async void Button_OnClicked(object sender, EventArgs e)
         {
             var inkomsten = new SQL_Inkomsten { }; //link with table
             inkomsten.Date = DateTime.Now;
             inkomsten.Value = Convert.ToDouble(Bedrag.Text, System.Globalization.CultureInfo.InvariantCulture);
             inkomsten.Category = Category.SelectedItem.ToString();
+            await _connection.InsertAsync(inkomsten);             await DisplayAlert("Alert", "Inkomsten succesvol toegevoegd", "OK");             await Navigation.PushAsync(new BudgetBuddyPage());             Navigation.RemovePage(this);
         }
-	}
+    }
 }
