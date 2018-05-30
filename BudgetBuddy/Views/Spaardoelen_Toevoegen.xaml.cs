3fa69211bebbd5f7681ea1a89e9ed032c061b74e
jurre <0891278@hr.nl>
2018-05-30 12:00:57 +0200

    Cleared Spaardielen_Toevoegen Pagina

---
+++ BudgetBuddy/Views/Spaardoelen_Toevoegen.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BudgetBuddy.Views
+{
+    public partial class Spaardoelen_Toevoegen : ContentPage
+    {
+        public Spaardoelen_Toevoegen()
+        {
+            InitializeComponent();
+        }
+    }
+}
