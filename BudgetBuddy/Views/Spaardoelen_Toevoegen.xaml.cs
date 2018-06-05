1e570b7fafe4a7e9ea51ed73a7c1e9f91a742252
Jurre1996 <jurre@koetse.eu>
2018-06-06 01:03:55 +0200

    Auto stash before merge of "master" and "origin/master"
    Added day counting logic to Spaardoel_Toevoegen

--- BudgetBuddy/Views/Spaardoelen_Toevoegen.xaml.cs
+++ BudgetBuddy/Views/Spaardoelen_Toevoegen.xaml.cs
@@ -7,11 +7,27 @@ namespace BudgetBuddy.Views
 {
     public partial class Spaardoelen_Toevoegen : ContentPage
     {
+        
         public Spaardoelen_Toevoegen()
         {
             InitializeComponent();
 
             //WHY U MERGE NO RIGHT VISUAL STUDIO?
+        }
+
+
+        private void DatePicker_OnDateSelected(object sender, DateChangedEventArgs e)
+        {
+
+
+            DateTime daysLeft = DatePickerSpaardoel.Date;
+
+            daysLeft.Subtract(DateTime.Today);
+
+            DaysLeft.Text = "U heeft nog: " + daysLeft.Day.ToString() + " dagen om u doel te bereiken.";
+
+
+
         }
     }
 }
