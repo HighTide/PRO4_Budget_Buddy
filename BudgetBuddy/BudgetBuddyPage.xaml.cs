73c12041e9c1e75ebaee2979f129358a95f3b09b
Rene van Til <noneofyourubssines>
2018-06-06 10:11:22 +0200

    Fixed Merge Conflict

--- BudgetBuddy/BudgetBuddyPage.xaml.cs
+++ BudgetBuddy/BudgetBuddyPage.xaml.cs
@@ -12,8 +12,8 @@ namespace BudgetBuddy
         private SQLiteAsyncConnection _connection;
         public string Button1Val = "Inkomsten";
         public string Button2Val = "Uitgaven";
-        public string Button3Val = "Overzicht";
-        public string Button4Val = "Settings";
+        public string Button3Val = "Spaardoelen";
+        public string Button4Val = "Overzicht";
         private ObservableCollection<SQL_Uitgaven> _uitgaven_maand_filter;
         private ObservableCollection<SQL_Uitgaven> _laatste_uitgave_filter;
         private DateTime _datum = DateTime.UtcNow.AddDays(-4);
@@ -22,7 +22,7 @@ namespace BudgetBuddy
         {
             _connection = DependencyService.Get<ISQLiteDb>().GetConnection();
             InitializeComponent();
-            
+
         }
 
         protected override async void OnAppearing()
@@ -44,8 +44,8 @@ namespace BudgetBuddy
             _laatste_uitgave_filter = new ObservableCollection<SQL_Uitgaven>(laatste_uitgave);
             //uitgaveView.ItemSource = _laatste_uitgave_filter;
 
-            var uitgaven_maand = await _connection.QueryAsync<SQL_Uitgaven>("SELECT SUM(Value) FROM SQL_Uitgaven WHERE Date <= ? AND Date >= ? LIMIT 1", 
-                new DateTime(DateTime.Now.Year, DateTime.Now.Month,1).Ticks, new DateTime(DateTime.Now.Year, DateTime.Now.Month,
+            var uitgaven_maand = await _connection.QueryAsync<SQL_Uitgaven>("SELECT SUM(Value) FROM SQL_Uitgaven WHERE Date <= ? AND Date >= ? LIMIT 1",
+                new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1).Ticks, new DateTime(DateTime.Now.Year, DateTime.Now.Month,
                              DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month)).Ticks);
 
             var uitgaven = await _connection.Table<SQL_Uitgaven>().Where(x => x.Date > _datum).ToListAsync();
@@ -63,10 +63,11 @@ namespace BudgetBuddy
 
 
             var buttons = await _connection.Table<SQL_Buttons>().ToListAsync();
-            foreach (var item in buttons){
+            foreach (var item in buttons)
+            {
                 if (item.Name == "Button1")
                 {
-                    Button1Val = item.Value;            
+                    Button1Val = item.Value;
                 }
 
                 if (item.Name == "Button2")
@@ -95,29 +96,89 @@ namespace BudgetBuddy
         void Text_Click(object sender, System.EventArgs e)
         {
             DisplayAlert("Budget Buddy", "Test", "OK");
-            
+
         }
 
 
 
-        async void Naar_Inkomen(object sender, System.EventArgs e)
+        async void Button_Pressed1(object sender, System.EventArgs e)
         {
-            await Navigation.PushAsync(new Inkomsten());
+            if (Button1.Text == "Uitgaven")
+            {
+                await Navigation.PushAsync(new Uitgaven());
+            }
+            if (Button1.Text == "Inkomsten")
+            {
+                await Navigation.PushAsync(new Inkomsten());
+            }
+            if (Button1.Text == "Spaardoelen")
+            {
+                await Navigation.PushAsync(new Spaardoelen());
+            }
+            if (Button1.Text == "Overzicht")
+            {
+                await Navigation.PushAsync(new Overzicht());
+            }
         }
 
-        async void Naar_Uitgaven(object sender, System.EventArgs e)
+        async void Button_Pressed2(object sender, System.EventArgs e)
         {
-            await Navigation.PushAsync(new Uitgaven());
+            if (Button2.Text == "Uitgaven")
+            {
+                await Navigation.PushAsync(new Uitgaven());
+            }
+            if (Button2.Text == "Inkomsten")
+            {
+                await Navigation.PushAsync(new Inkomsten());
+            }
+            if (Button2.Text == "Spaardoelen")
+            {
+                await Navigation.PushAsync(new Spaardoelen());
+            }
+            if (Button2.Text == "Overzicht")
+            {
+                await Navigation.PushAsync(new Overzicht());
+            }
         }
 
-        async void Overzicht(object sender, System.EventArgs e)
+        async void Button_Pressed3(object sender, System.EventArgs e)
         {
-			await Navigation.PushAsync(new Overzicht());
+            if (Button3.Text == "Uitgaven")
+            {
+                await Navigation.PushAsync(new Uitgaven());
+            }
+            if (Button3.Text == "Inkomsten")
+            {
+                await Navigation.PushAsync(new Inkomsten());
+            }
+            if (Button3.Text == "Spaardoelen")
+            {
+                await Navigation.PushAsync(new Spaardoelen());
+            }
+            if (Button3.Text == "Overzicht")
+            {
+                await Navigation.PushAsync(new Overzicht());
+            }
         }
 
-        async void Button_Clicked(object sender, System.EventArgs e)
+        async void Button_Pressed4(object sender, System.EventArgs e)
         {
-            await Navigation.PushAsync(new Settings());
+            if (Button4.Text == "Uitgaven")
+            {
+                await Navigation.PushAsync(new Uitgaven());
+            }
+            if (Button4.Text == "Inkomsten")
+            {
+                await Navigation.PushAsync(new Inkomsten());
+            }
+            if (Button4.Text == "Spaardoelen")
+            {
+                await Navigation.PushAsync(new Spaardoelen());
+            }
+            if (Button4.Text == "Overzicht")
+            {
+                await Navigation.PushAsync(new Overzicht());
+            }
         }
     }
 }
