44f52cb9acb944ef1f29d5b5038500cbfb8eee29
Floor <floorvermaat@hotmail.com>
2018-06-06 10:13:58 +0200

    Orientation screen Portrait

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 
 namespace BudgetBuddy.Droid
 {
-    [Activity(Label = "Budget Buddy", Icon = "@drawable/icon_android", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Budget Buddy", Icon = "@drawable/icon_android", Theme = "@style/MyTheme", ScreenOrientation = ScreenOrientation.Portrait, MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
