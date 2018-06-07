dc55411e5535a27fe5a4c660d999da921a107b68
René van Til <ploxdontspamme>
2018-06-07 23:19:12 +0200

    Fixed totaaloverezicht balans not changing correctly when deleting items

--- BudgetBuddy/Views/Totoverzicht.xaml.cs
+++ BudgetBuddy/Views/Totoverzicht.xaml.cs
@@ -26,7 +26,7 @@ namespace BudgetBuddy.Views
 
         protected override async void OnAppearing()
         {
-            var Tots = await _connection.QueryAsync<SQL_Transacties>("SELECT * FROM SQL_Transacties ORDER BY Date DESC");
+            var Tots = await _connection.QueryAsync<SQL_Transacties>("SELECT * FROM SQL_Transacties WHERE NOT Recurring ORDER BY Date DESC");
 
             Total.ItemsSource = Tots;
 
@@ -48,18 +48,22 @@ namespace BudgetBuddy.Views
 
         async void MenuItem_Clicked(object sender, System.EventArgs e)
         {
+            totalis2 = 0.00;
             var mi = ((MenuItem)sender);
             var viewCellSelected = sender as MenuItem;
-            var calculationToDelete = viewCellSelected?.BindingContext;
-
+            var calculationToDelete = viewCellSelected?.BindingContext as SQL_Transacties;
+            if (calculationToDelete.Value > 0)
+                await _connection.ExecuteAsync("Update SQL_Budget SET Value = Value - ? Where Name = ?", calculationToDelete.Value, "Budget");
+            else if (calculationToDelete.Value < 0)
+                await _connection.ExecuteAsync("Update SQL_Budget SET Value = Value + ? Where Name = ?", Math.Abs(calculationToDelete.Value), "Budget");
             await _connection.DeleteAsync(calculationToDelete);
-            var Tots = await _connection.QueryAsync<SQL_Transacties>("SELECT * FROM SQL_Transacties ORDER BY Date DESC");
+            var Tots = await _connection.QueryAsync<SQL_Transacties>("SELECT * FROM SQL_Transacties WHERE NOT Recurring ORDER BY Date DESC");
             Total.ItemsSource = Tots;
             foreach (var item in Tots)
             {
                 totalis2 += item.Value;
             }
-            if (totalis2 > 0)
+            if (totalis2 >= 0)
             {
                 Totals.TextColor = Color.LawnGreen;
             }
